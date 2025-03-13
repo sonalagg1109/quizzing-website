71072fdd58082dd2f2f5926b722177ebc07accc9
Sonal Agrawal <145599106+sonalagg1109@users.noreply.github.com>
2025-03-13 22:25:53 +0530

    Delete src directory

--- src/components/atoms/Form.jsx
+++
@@ -1,9 +0,0 @@
-function Form({ children, className, ...rest }) {
-  return (
-    <form className={`grid h-3/4 grid-cols-1 ${className}`} {...rest}>
-      {children}
-    </form>
-  );
-}
-
-export default Form;
